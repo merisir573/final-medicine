703fb425dd6d5341bbdc9948dfab70f3e05d4304
merisir573 <aikosstory@gmail.com>
2025-01-30 12:37:16 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allow all origins
+  });
   await app.listen(3003);
 }
 bootstrap();
